16c38cd1f527335fd3f8d4334cbeb09af073eb40
Raja Babu <rajababu1259@gmail.com>
2022-03-14 23:51:31 +0530

    Add angular routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { LoginSuccessfulComponent } from './login-successful/login-successful.component';
+import { RegisterComponent } from './register/register.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { RegisterSuccessfulComponent } from './register-successful/register-successful.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'login-successful', component: LoginSuccessfulComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'register-successful', component: RegisterSuccessfulComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
